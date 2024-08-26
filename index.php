2bc645f8b2e19957982b602b821399734751a6ad
leonamalexandria <leonamalexandria@gmail.com>
2024-08-25 22:46:38 -0300

    Criando

--- index.php
+++ index.php
@@ -63,33 +63,6 @@
 			</div>
 		</div><!--center-->
 	</section>
-
-<script src="js/jquery.js"></script>
-<script src="js/slick.min.js"></script>
-
-<script type="text/javascript">
-
-	$('section.clientes-slider .slider-container').slick({
-		dots: true,
-		arrows:false,
-	    infinite: false,
-	    speed:1000,
-	    slidesToShow: 4,
-	    autoplay: true,
-	    centerMode:false,
-	    autoplaySpeed: 3000,
-	    pauseOnHover:false,
-	    responsive: 
-	    [
-	    {
-	      breakpoint: 768,
-	      settings: {
-	        slidesToShow: 2
-	      }
-	    }
-	    ]
-	});
-</script>
 	<div class="clear"></div>
 	<br />
 
@@ -127,11 +100,100 @@
 					<img src="images/php-elephant.jpg" />
 					<h3>Site sobre tal projeto3</h3>
 				</div><!--box-single-sites-->
-
 			</div><!--sites-->
-
+		<div class="clear"></div>
 		</div><!--center-->
 	</section><!--projetos-->
+
+	<section class="depoimentos">
+		<div class="center">
+			<h2>Depoimentos</h2>
+			<div class="depoimento-box">
+				<div class="depoimento-single">
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+					<p>Cadu Maverick</p>
+					<img src="images/php-elephant.jpg">
+				</div><!--Depoimento-single-->
+				<div class="depoimento-single">
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+					<p>Cadu Maverick</p>
+					<img src="images/php-elephant.jpg">
+				</div><!--Depoimento-single-->
+				<div class="depoimento-single">
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+					<p>Cadu Maverick</p>
+					<img src="images/php-elephant.jpg">
+				</div><!--Depoimento-single-->
+			</div><!--Depoimento-box-->
+		</div><!--Center-->
+	</section><!--Depoimentos-->
+
+	<footer>
+		<section class="metodologia">
+			<div class="center">
+				<h2>Conheça nossa Metodologia</h2>
+				<p>O que acha de fazermos o que mais gostamos de fazer? Conversar? <br />Entre em contato pelo Email </p>
+				<a href="/contato">Entrar em contato</a>
+			</div><!--Center-->
+		</section><!--Metodologia-->
+
+		<div class="center">
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">contato</a>
+				<a href="">FAQ</a>
+			</div><!--col--footer-->
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">contato</a>
+				<a href="">FAQ</a>
+			</div><!--col--footer-->
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">contato</a>
+				<a href="">FAQ</a>
+			</div><!--col--footer-->
+			<div style="width: 40%; text-align: right;" class="col-footer">
+				<img src="images/php-elephant.jpg">
+			</div><!--col--footer-->
+		</div><!--Center-->
+	</footer>
+
+<script src="js/jquery.js"></script>
+<script src="js/slick.min.js"></script>
+
+<script type="text/javascript">
+
+	$('section.clientes-slider .slider-container').slick({
+		dots: true,
+		arrows:false,
+	    infinite: false,
+	    speed:1000,
+	    slidesToShow: 4,
+	    autoplay: true,
+	    centerMode:false,
+	    autoplaySpeed: 3000,
+	    pauseOnHover:false,
+	    responsive: 
+	    [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 2
+	      }
+	    }
+	    ]
+	});
+
+	$('section.depoimentos .depoimento-box').slick({
+		dots: true,
+		arrows:false,
+	    infinite: true,
+	    speed:1000,
+	    slidesToShow: 1,
+	    centerMode:false,
+	});
+</script>
 
 <script>
 	//Menu responsivo.
---
+++ sobre.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="fonts/Roboto" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/0ac4ca22f0.js" crossorigin="anonymous"></script>
+
+	<title>Leonam - tema</title>
+</head>
+<body>
+
+	<section class="topo">
+		<div class="center">
+
+			<header>
+				<div class="logo">Leonam Alexandria</div><!--logo-->
+			</header>
+
+			<ul class="menu-desktop">
+				<li><a href="">Home</a></li>
+				<li><a href="">English version</a></li>
+				<li><a href="">Contato</a></li>
+			</ul>
+
+			<div class="menu-mobile">
+				<i class="fa-solid fa-bars"></i>
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">English version</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</div><!--menu-mobile-->
+			<div class="clear"></div>
+		</div><!--center-->
+	</section><!--topo-->
+
+	<section class="sobre-mim">
+		<div class="center">
+			<div class="w50 sobre-mim-texto">
+				<h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h2>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			</div><!--w50-->
+			<div class="w50 sobre-mim-imagem">
+				<img src="images/IMG1.jpg">
+			</div><!--w50 seobre-mim-imagem-->
+			<div class="clear"></div>
+		</div><!--Center-->
+	</section><!--sobre-mim-->
+
+	<section class="sobre-img">
+		<div class="center">
+			<div class="img-sobre">
+				<img src="images/php.jpg" />
+			</div><!--img-sobre-->
+			<div class="img-sobre">
+				<img src="images/php.jpg" />
+			</div><!--img-sobre-->
+			<div class="img-sobre">
+				<img src="images/php.jpg" />
+			</div><!--img-sobre-->
+			<div class="img-sobre">
+				<img src="images/php.jpg" />
+			</div><!--img-sobre-->
+		</div><!--Center-->
+	</section><!--Sobre-img-->
+
+	<footer>
+		<section class="metodologia">
+			<div class="center">
+				<h2>Conheça nossa Metodologia</h2>
+				<p>O que acha de fazermos o que mais gostamos de fazer? Conversar? <br />Entre em contato pelo Email </p>
+				<a href="/contato">Entrar em contato</a>
+			</div><!--Center-->
+		</section><!--Metodologia-->
+
+		<div class="center">
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">contato</a>
+				<a href="">FAQ</a>
+			</div><!--col--footer-->
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">contato</a>
+				<a href="">FAQ</a>
+			</div><!--col--footer-->
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">contato</a>
+				<a href="">FAQ</a>
+			</div><!--col--footer-->
+			<div style="width: 40%; text-align: right;" class="col-footer">
+				<img src="images/php-elephant.jpg">
+			</div><!--col--footer-->
+		</div><!--Center-->
+	</footer>
+
+
+</body>
+</html>
